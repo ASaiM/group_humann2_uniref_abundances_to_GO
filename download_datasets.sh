61acba68b4fe4c842b54b3fb1c418cba20abc0c1
bebatut <berenice.batut@gmail.com>
2016-02-04 10:44:58 +0100

    Correct some minor errors in download_datasets
    - Add path
    - Move echo

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-data_dir="data/"
+MY_PATH=`dirname "$0"`
+data_dir=$MY_PATH"/data/"
 
 if [ ! -d $data_dir ]; then
     mkdir $data_dir
@@ -8,16 +9,17 @@ fi
 
 cd $data_dir
 
-echo "Download Gene Ontology (basic and metagenomic slim)..."
 if [ ! -f "go.obo" ]; then
+    echo "Download basic Gene Ontology..."
     wget http://geneontology.org/ontology/go.obo
 fi 
 if [ ! -f "goslim_metagenomics.obo" ]; then
+    echo "Download metagenomic slim Gene Ontology..."
     wget http://www.geneontology.org/ontology/subsets/goslim_metagenomics.obo
 fi
 
-echo "Download humann2 correspondance between Uniref50 and GO"
 if [ ! -f "map_infogo1000_uniref50.txt" ]; then
+    echo "Download humann2 correspondance between Uniref50 and GO"
     wget https://bitbucket.org/biobakery/humann2/raw/d1ac153083d48a5384e3b3d3597b9e36b1a4606e/humann2/data/misc/map_infogo1000_uniref50.txt.gz
     gunzip map_infogo1000_uniref50.txt.gz
 fi
